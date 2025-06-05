5bb66e9561a884766c9416902bfb31c32e37ec19
Johanna Schnell <johannaschnell@gmail.com>
2025-06-05 14:56:46 -0700

    fix: merge conflict

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -62,11 +62,3 @@ export function ThemeProvider({
     </ThemeProviderContext.Provider>
   );
 }
-
-// export const useTheme = () => {
-//   const context = useContext(ThemeProviderContext);
-
-//   if (context === undefined) throw new Error("useTheme must be used within a ThemeProvider");
-
-//   return context;
-// };
