736fcecebe18d038c1412df42efe7209bd8ad12c
Johanna Schnell <johannaschnell@gmail.com>
2025-06-04 17:58:16 -0700

    feat: user-management-page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils";
 import { buttonVariants } from "./button-variants";
 
-import { cn } from "@/lib/utils"
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
