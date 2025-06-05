d0ebeed164c818c08a3fa26e0fa4db0d6b26d937
Johanna Schnell <johannaschnell@gmail.com>
2025-06-04 17:49:16 -0700

    feat: user-management-page

---
+++ src/components/User Management Page/user-management.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import UserManagementPage from './user-management';
+import { ThemeProvider } from '@/components/theme-provider';
+
+const meta: Meta<typeof UserManagementPage> = {
+  title: 'Pages/UserManagementPage',
+  component: UserManagementPage,
+    decorators: [
+     (Story) => <ThemeProvider><Story /></ThemeProvider>,
+   ],
+};
+export default meta;
+
+type Story = StoryObj<typeof UserManagementPage>;
+
+export const Default: Story = {
+  render: () => <UserManagementPage />,
+};
