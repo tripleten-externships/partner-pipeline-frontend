292df1b4f16de56f1ea54a016029844f73ce2f4e
Alex Sturm <sturmgegencoding@gmail.com>
2025-07-02 23:56:16 -0500

    feature: added global permission handling with zustand and protected change role section to admin only

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,20 +11,4 @@ const login = ({ email, password }: { email: string; password: string }) => {
 
 //could be further refined by creating validation through typescript for email type, but I'm expecting we'll address that with user entry and backend validation
 
-//  const onLogin = ({ email, password }: { email: string; password: string}) => {
-//     const makeRequest = () => {
-//       if (!email || !password) {
-//         return;
-//       }
-//       return auth.login({ email, password }).then((data) => {
-//         if (data.token) {
-//           setToken(data.token);
-//           //room for other functionality (set user, navigate, etc)
-//         }
-//       });
-//     };
-//     makeRequest;
-//   };
-// ^Function pre built to place into app. Logs in user and sets token
-
 export { login };
