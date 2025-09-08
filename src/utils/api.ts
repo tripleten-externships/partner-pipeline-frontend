df27c73def090e282d3c5257228eac357dc149ae
Georgia <georgialloydphoto@gmail.com>
2025-09-08 14:58:02 -0500

    feat: ppc4-5 post milestone api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,35 @@
-function processServerRequest(res: Response) {
-  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-}
-//helpful function pair for shorthand on requests to check for errors
-
 const baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
 
 const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
 };
-//making an assumption for headers
 
-export { processServerRequest, baseUrl, headers };
+type Milestone = {
+  title: string;
+  status: string;
+  description: string;
+};
+
+//helpful function pair for shorthand on requests to check for errors
+function processServerRequest(res: Response) {
+  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+}
+
+// API client function to fetch data from the backend.
+function requestData(url: string, options?: RequestInit) {
+  return fetch(url, options).then(processServerRequest);
+}
+
+// a dedicated frontend API function that sends a POST request
+function addMilestone(milestone: Milestone) {
+  return requestData(`${baseUrl}/api/projects/:projectID/milestones`, {
+    method: "POST",
+    headers: {
+      ...headers,
+    },
+    body: JSON.stringify(milestone),
+  });
+}
+
+export { baseUrl, headers, processServerRequest, requestData, addMilestone };
