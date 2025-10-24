a33590ecc613a822358e1302499085b9562ee72d
Jackie Lee <jackieleeid2000@gmail.com>
2025-10-24 14:31:13 -0600

    feat: connect sending account info to api

--- src/components/invite-route.tsx
+++ src/components/invite-route.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { InviteFormModal} from "../components/InviteFormModal/InviteFormModal";
-
+// import {sendUserInvitation} from "@/utils/api"
 
 
 function Login() {
@@ -18,17 +18,20 @@ type Account = {
 //Invitation Form Modal Open or closed
 const [openModal, setOpenModal] = useState(true); //Set to true if always open
 
-//Default Account that is premade can be null in the future
-const [AccountsList, setAccountsList] = useState<Account[]>([
+//Default list of Accounts that is premade can be null (empty) in the future
+const [AccountsList] = useState<Account[]>([
     { name: "Joe", email: "Joe@gmail.com", role: "admin" },
   ]);
 
 
   //Called upon succesful submit of Invitation Form (what happens after pressing submit)
    const handleAddAccount = (newAccount: Account) => {
-    setAccountsList((prev) => [...prev, newAccount]); //add account to list
+    // setAccountsList((prev) => [...prev, newAccount]); //add account to list
+    // sendUserInvitation("10", newAccount);
+    console.log(newAccount);
     console.log(AccountsList);
   };
+
   return (
     <>
     {/* Modal */}
