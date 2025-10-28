f8903e053d832e71e3ce43b56c051229b45f28c1
Jackie Lee <jackieleeid2000@gmail.com>
2025-10-28 15:37:26 -0600

    feat: sidebar and modal on invite route

--- src/components/invite-route.tsx
+++ src/components/invite-route.tsx
@@ -1,71 +1,8 @@
 import React, {useState} from 'react';
-import { DashProps } from "@/utils/types";
+import { DashProps } from "@/utils/types"; //Give all the props their specfied types to implement Sidebar correctly
 import Sidebar from './Sidebar/Sidebar';
 import { InviteFormModal} from "../components/InviteFormModal/InviteFormModal";
 import {sendUserInvitation} from "@/utils/api"
-
-
-// function SendInvitePage( projectList) {
-
-// //Roles for the new accounts
-// type AccessLevel = "admin" | "student";
-
-//Each Account has a name, email, and role as filled in through the form
-// type Account = {
-//   name: string;
-//   email: string;
-//   role: AccessLevel;
-// }
-
-
-
-// //Invitation Form Modal Open or closed
-// const [openModal, setOpenModal] = useState(true); //Set to true if always open
-
-// //Default list of Accounts that is premade can be null (empty) in the future
-// // const [AccountsList] = useState<Account[]>([
-// //     { name: "Joe", email: "Joe@gmail.com", role: "admin" },
-// //   ]);
-
-
-  //Called upon succesful submit of Invitation Form (what happens after pressing submit)
-  //  const handleAddAccount = (newAccount: Account) => {
-  //   console.log(newAccount);
-  //   sendUserInvitation("10", {name: newAccount.name, email: newAccount.email, roleToGrant: newAccount.role});
-  // };
-
-//   return (
-//     <>
-//         <Sidebar
-//       projectList={projectList}
-//       loadingProjects={loadingProjects}
-//       projectError={projectError}
-//       selectedProjectId={selectedProjectId}
-//       isProjectDropdownOpen={isProjectDropdownOpen}
-//       toggleProjectDropdown={toggleProjectDropdown}
-//       projectDropdownRef={projectDropdownRef}
-//       handleProjectSelect={handleProjectSelect}
-//       openMenus={openMenus}
-//       setOpenMenus={setOpenMenus}
-//       isUserMenuOpen={isUserMenuOpen}
-//       toggleUserMenu={toggleUserMenu}
-//       userMenuRef={userMenuRef}
-//       userEmail={userEmail}
-//       toggleMenu={toggleMenu}
-//       isAddProjectSheetOpen={isAddProjectSheetOpen}
-//       setIsAddProjectSheetOpen={setIsAddProjectSheetOpen}
-//     />
-//     {/* Modal */}
-//         <InviteFormModal
-//             open={openModal}
-//             onClose={() => setOpenModal(false)}
-//             onCreate={handleAddAccount}
-//         />
-//     </>
-//   );
-// }
-
-// export default SendInvitePage;
 
 const SendInvitePage: React.FC<DashProps> 
 = ({
@@ -126,13 +63,6 @@ const SendInvitePage: React.FC<DashProps>
         />
       </div>
 
-      //     {/* Modal */}
-//         <InviteFormModal
-//             open={openModal}
-//             onClose={() => setOpenModal(false)}
-//             onCreate={handleAddAccount}
-//         />
-//     </>
 );
 
 };
