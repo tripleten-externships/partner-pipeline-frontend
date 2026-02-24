657306d955a5471319af47bd7cbb11633ebeaaa7
Jeremiah P <speceod@gmail.com>
2026-02-24 15:12:27 -0800

    fix: delete parts that caused errors

--- src/components/StudentStatusModal/StudentStatusModal.tsx
+++ src/components/StudentStatusModal/StudentStatusModal.tsx
@@ -57,17 +57,17 @@ const StudentStatusModal: React.FC<StudentStatusModalProps> = ({isOpen, onClose,
     if (!isOpen) return null;
 
     return(
-        <div className="flex h-full w-full inset-0 fixed justify-center items-center">
+        <div className="flex bg-white-50 h-full w-full inset-0 fixed justify-center items-center">
             <div className="flex flex-col bg-white w-[600px] h-[650px] p-5 rounded-[10px]" >
                 <button onClick={onClose} className="h-[15px] w-[15px] relative top-0 left-[550px]">
                     <img src={modalCloseButton} alt="close button" />
                 </button>
                 <h2 className="text-black pb-[5px] font-semibold text-larger">{student.id}</h2>
-                <p className="text-[#808080] pb-[5px]">View and edit student information</p>
+                <p className="text-[#808080] pb-[20px] pr-[100px]">View and edit student information</p>
                 <form onSubmit={handleSubmit}><div className="flex ">
                     <div className="m-0 flex flex-col">
                     <label className="text-black pb-[5px] font-medium"> Email</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]">{student.email}</p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]">{student.email}</p>
 
                     <label className="text-black pb-[5px] font-medium">Program</label>
                     <p className="text-black rounded-[8px] bg-white border-[#808080] border-solid w-fit mb-5"> {student.program}
@@ -80,21 +80,21 @@ const StudentStatusModal: React.FC<StudentStatusModalProps> = ({isOpen, onClose,
                         <option value="UX/UI" className="text-[#808080] pb-5 pb-[100px]">UX/UI</option> */}
                     </p>
                     <label className="text-black pb-[5px] font-medium">Completion Date</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]">{student.completionDate}</p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]">{student.completionDate}</p>
                     <label className="text-black pb-[5px] font-medium">Last Contact</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]">{student.lastContactDate}</p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]">{student.lastContactDate}</p>
                     <label className="text-black pb-[5px] font-medium"> Hub Profile</label>
                     <button onClick={handleProfileLinkClick} className="text-[#808080] w-fit pb-5"> View Profile </button>
                     </div>
                     <div className="m-0 flex flex-col">
                     <label className="text-black pb-[5px] font-medium">Status</label>
-                    <p className="text-white rounded-[8px] bg-black w-fit py-[5px] px-8px] mb-5">{student.status}</p>
+                    <p className="text-white rounded-[8px] bg-black w-fit py-[5px] px-[8px] mb-5">{student.status}</p>
                     <label className="text-black pb-[5px] font-medium">Sent Invites</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]">{student.invitesSent}</p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]">{student.invitesSent}</p>
                     <label className="text-black pb-[5px] font-medium">Date Added</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]">{student.dateAdded}</p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]">{student.dateAdded}</p>
                     <label className="text-black pb-[5px] font-medium">Voucher Issued</label>
-                    <p className="text-[#808080] pb-5 pr-[100px]"> -- </p>
+                    <p className="text-[#808080] pb-[20px] pr-[100px]"> -- </p>
                     </div>
                     </div>
                     <label className="text-black pb-[5px] font-medium">Notes</label>
@@ -105,7 +105,7 @@ const StudentStatusModal: React.FC<StudentStatusModalProps> = ({isOpen, onClose,
                         placeholder="Add notes about this student..." 
                         max="100" className="text-[#808080] border-solid rounded-[8px] h-[95px] w-full leading-[12px]"
                     />
-                    <button type="submit" className="text-white bg-black rounded-[10px] h-[40px] w-[65px] mt-[5px] ml-[490px]" disabled={isSaving}>{isSaving ? "Saving..." : "Save" }</button>
+                    <button type="submit" className="text-white !bg-black rounded-[10px] h-[40px] w-[65px] mt-[5px] ml-[490px]" disabled={isSaving}>{isSaving ? "Saving..." : "Save" }</button>
                 </form>
 
             </div>
